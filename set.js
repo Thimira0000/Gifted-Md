84a2f72abf3897564043d37267c5fd5953f3436d
Thimira0000 <thimiradenuwan11@gmail.com>
2024-06-01 15:24:17 +0530

    Update set.js

--- set.js
+++ set.js
@@ -9,13 +9,13 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
     : process.env.DATABASE_URL;
 module.exports = { session: process.env.SESSION_ID || 'Gifted;;;',
     PREFIXE: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "Gifted Tech",
-    NUMERO_OWNER : process.env.OWNER_NUMBER || "254762016957,254728782591", 
+    OWNER_NAME: process.env.OWNER_NAME || "ᴘᴀɴᴅᴀ ʙʙʜ ᴍᴅ",
+    NUMERO_OWNER : process.env.OWNER_NUMBER || "94719403748,94717381136", 
              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
-AUTOREAD_MESSAGES: process.env.AUTO_READ_MESSAGES || "no",
+AUTOREAD_MESSAGES: process.env.AUTO_READ_MESSAGES || "ni",
 CHATBOT: process.env.CHAT_BOT || "no",
-    AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
+    AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'yes',
     BOT : process.env.BOT_NAME || 'ɢɪғᴛᴇᴅ-ᴍᴅ',
     OPENAI_API_KEY : process.env.OPENAI_API_KEY || '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',
     URL : process.env.BOT_MENU_LINKS || 'https://telegra.ph/file/a202f454c9532c3f5b7f8.jpg',
